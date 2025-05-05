17f2c15480fe0692a54266c35b90dd7dbb5e37d3
AdamRB208 <adamrborup@gmail.com>
2025-05-05 10:57:55 -0600

    added Rat model and added to db context

---
+++ src/models/Rat.js
@@ -0,0 +1,12 @@
+import { Schema } from "mongoose";
+
+export const RatSchema = new Schema({
+  name: { type: String, minLength: 1, maxLength: 100, required: true },
+  callsign: { type: String, minLength: 1, maxLength: 100, required: true },
+  picture: { type: String, minLength: 1, maxLength: 100, required: true },
+  specialties: { type: String, minLength: 1, maxLength: 100, required: true }
+},
+  {
+    timestamps: true,
+    toJSON: { virtuals: true }
+  })
